7b11da3c8f2cc0afe5e200289f0e16534de9bc0c
Eino <e.i.korte@gmail.com>
2020-03-11 21:27:10 +0200

    added select feature

--- app/world/map.py
+++ app/world/map.py
@@ -1,4 +1,6 @@
+from enum import Enum
 from .tile import Tile
+
 class Map:
     def __init__(self, w,h):
         self.w = w
@@ -6,6 +8,8 @@ class Map:
         self.seedLoc = (14,7)
         self.selectLoc = [self.seedLoc[0], self.seedLoc[1]]
         self.map = [[]]
+        self.weather = Weather.SUNNY.value
+        self.year = 0
         for row in range(self.w):
             self.map.append([])
             for elem in range(self.h):
@@ -19,11 +23,28 @@ class Map:
     def grow(self):
         self.selectLoc[0]-=1
         self.map[self.selectLoc[0]][self.selectLoc[1]].changeTo('|')
+        self.select(self.selectLoc[0], self.selectLoc[1])
     def branch(self, dir):
         if dir == 'left':
+            parent = self.map[self.selectLoc[0]][self.selectLoc[1]]
             self.selectLoc[1]-=1
-            self.map[self.selectLoc[0]][self.selectLoc[1]].changeTo('\\')
+            self.map[self.selectLoc[0]][self.selectLoc[1]].changeTo('-')
+            self.map[self.selectLoc[0]][self.selectLoc[1]].setParent(parent)
+            self.select(self.selectLoc[0], self.selectLoc[1])
         elif dir == 'right':
+            parent = self.map[self.selectLoc[0]][self.selectLoc[1]]
             self.selectLoc[1]+=1
-            self.map[self.selectLoc[0]][self.selectLoc[1]].changeTo('/')
+            self.map[self.selectLoc[0]][self.selectLoc[1]].changeTo('-')
+            self.map[self.selectLoc[0]][self.selectLoc[1]].setParent(parent)
+            self.select(self.selectLoc[0], self.selectLoc[1])
+    def select(self, x,y):
+        for row in self.map:
+            for elem in row:
+                elem.selected = False
 
+        self.selectLoc[0] = x
+        self.selectLoc[1] = y
+
+        self.map[x][y].selected = True
+class Weather(Enum):
+    SUNNY = 'sunny'
